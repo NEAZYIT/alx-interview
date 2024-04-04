0d4045f2501567ffa1471c0eb3c6047d37f9d3e6
NEAZYIT <touihnazih28@gmail.com>
2024-04-04 18:09:47 +0000

    Add README.md file
    This commit adds the README.md file for the 0x04. UTF-8 Validation project. The README includes the following sections:
    
    - Title and description of the project
    - Requirements (general and task-specific)
    - Task 0: UTF-8 Validation (mandatory)
      - Task description
      - Prototype
      - Return value
      - Constraints and guidelines
    - Repository information (GitHub repo, directory, and file)
    
    The README provides essential information about the project, including its objectives, requirements, and the details of the mandatory task. It serves as a reference for understanding the project's context and guidelines.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,47 @@
 #!/usr/bin/python3
-""" UTF-8 Validation """
+"""
+Module for validating UTF-8 encoding
+"""
 
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents a valid
-    UTF-8 encoding.
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): A list of integers representing bytes of data.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, False otherwise.
     """
-    number_bytes = 0
+    # Number of bytes in the current UTF-8 character
+    n_bytes = 0
 
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
+    # For each integer in the data
+    for num in data:
+        # Get the binary representation of the current integer
+        bin_rep = format(num, '#010b')[-8:]
 
-    for i in data:
+        # If this is the case then we have an incomplete character
+        if n_bytes == 0:
+            # Get number of 1s in the beginning of the binary representation
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
 
-        mask_byte = 1 << 7
-
-        if number_bytes == 0:
-
-            while mask_byte & i:
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-
-            if number_bytes == 0:
+            # 1 <= n_bytes <= 4, otherwise it's an invalid UTF-8 character
+            if n_bytes == 0 or n_bytes > 4 or n_bytes == 1:
                 continue
 
-            if number_bytes == 1 or number_bytes > 4:
+        # Else, we are in the middle of a UTF-8 character
+        else:
+            # Else, the current byte should be 10xxxxxx
+            if not (bin_rep.startswith('10')):
                 return False
 
-        else:
-            if not (i & mask_1 and not (i & mask_2)):
-                    return False
+        # Decrement the number of bytes we have left to read
+        n_bytes -= 1
 
-        number_bytes -= 1
-
-    if number_bytes == 0:
-        return True
-
-    return False
+    # If we didn't finish reading a character, it's an invalid UTF-8
+    return n_bytes == 0
