41b9441bb5041f0c1c0b9f0bae845da3c3137971
NEAZYIT <touihnazih28@gmail.com>
2024-05-03 06:18:49 -0700

    Implement dynamic programming solution for coin change problem
    This commit introduces the implementation of the makeChange function to solve the coin change problem using dynamic programming. The function efficiently determines the fewest number of coins needed to meet a given total amount using a bottom-up approach. Tests have been included to verify the correctness of the implementation.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
+""" Contains makeChange function"""
+
+
 def makeChange(coins, total):
-    # Initialize dp array
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Iterate through each coin denomination
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        # Iterate through range from coin to total + 1
-        for j in range(coin, total + 1):
-            # Update dp[j]
-            dp[j] = min(dp[j], dp[j - coin] + 1)
-
-    # Return result
-    return (dp[total] if dp[total] != float('inf') else -1)
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
